e51ba8d5af0f3226b6cb74476a41b29024d9bedd
thuviengithub0208 <khoaphp@yahoo.com>
2018-07-03 18:46:39 +0700

    Initial commit

---
+++ app/src/main/java/com/ptp/phamtanphat/listviewcoban1206/MainActivity.java
@@ -0,0 +1,45 @@
+package com.ptp.phamtanphat.listviewcoban1206;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    ArrayList<String> mangten;
+    ArrayAdapter arrayAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv = findViewById(R.id.listview);
+        mangten = new ArrayList<>();
+
+        mangten.add("Nguyen Van A");
+        mangten.add("Nguyen Van B");
+        mangten.add("Nguyen Van C");
+        mangten.add("Nguyen Van D");
+        mangten.add("Nguyen Van E");
+
+        //Ban ve cho listview
+        arrayAdapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,mangten);
+        lv.setAdapter(arrayAdapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, mangten.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
